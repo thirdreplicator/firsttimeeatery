b250a589151d8e4c4697a399ab9298a46819c71b
David Beckwith <thirdreplicator@gmail.com>
2023-03-16 21:51:41 +0800

    Prettied up the homepage. Got carousel working.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
-/// <reference types="astro/client" />
+/// <reference types="@astrojs/image/client" />
+interface ImportMetaEnv {
+  readonly DB_PASSWORD: 'asdf';
+  readonly PUBLIC_POKEAPI: 'wowee';
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
