d87a697a8c00029408d8c716e482a09aa8193da3
David Beckwith <thirdreplicator@gmail.com>
2023-04-09 08:38:33 +0800

    Remove facebook code

--- src/components/UserLoginDrawer.svelte
+++ src/components/UserLoginDrawer.svelte
@@ -22,22 +22,6 @@
     $currentUser = {}
     window.location = '/'
   }
-  
-  const loginWithFacebook = () => {
-    FB.login(function(response) {
-      if (response.status === 'connected') {
-        console.log('Logged into your webpage and Facebook.', response)
-        // 1. Save response on server. (find or create "User", then find/update row in fb_info and attach to "User")
-        // 2. Request user's first_name, last_name, full_name, picture, email, save response on server.
-        // 3. Display it on the current page.
-        // 4. Save it on the server.
-        // 5. Redirect to /menu.
-      } else {
-        console.log('The person is not logged into your webpage or we are unable to tell.')
-      }
-    }, {scope: 'public_profile'});
-  }
-
 
   const submitLoginForm = async  (e) => {
       e.preventDefault()
@@ -196,30 +180,6 @@
         </div>
 
   </div>
-  
-              <!-- Facebook button-->
-              <div class="w-full text-center">
-                <button type="button" on:click={ loginWithFacebook }
-                  class="w-60 text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2">
-                  <svg class="float-left w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-f" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M279.1 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.4 0 225.4 0c-73.22 0-121.1 44.38-121.1 124.7v70.62H22.89V288h81.39v224h100.2V288z"></path></svg>
-                  Sign in with Facebook
-                </button>
-              </div>
-
-              <!-- Google button -->
-              <div class="w-full text-center">
-                <button type="button" class="w-60 text-white bg-google hover:[#de5246]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
-                  <svg class="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>
-                  Sign in with Google
-                </button>
-              </div>
-
-              <div class="flex divider items-center justify-center">
-                <div class="flex-1 p-2"><hr /></div>
-                <div class="p-2 text-medium">Or&nbsp;</div>
-                <div class="flex-1 p-2"><hr /></div>
-              </div>
-              
               <form id="user-login-form" class="space-y-4 md:space-y-6" action="#">
                   <div>
                       <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
@@ -231,6 +191,7 @@
                       <input type="password" name="password" id="password" placeholder="••••••••"
                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                   </div>
+                  <!--
                   <div class="flex items-center justify-between">
                       <div class="flex items-start">
                           <div class="flex items-center h-5">
@@ -242,6 +203,7 @@
                       </div>
                       <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                   </div>
+                  -->
                   <div class="w-full text-center">
                     <button
                     type="submit"
